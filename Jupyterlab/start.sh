f9e65befe7ee74476c03f2c8fcd16d61e1cf335c
David Carlson <mndrake22@mac.com>
2018-06-17 11:02:17 -0500

    Update start.sh

--- Jupyterlab/start.sh
+++ Jupyterlab/start.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-set -o nounset -o errexit
+set -o nounset -o errexit -o pipefail
 
+IP_ADDR=$(/sbin/ifconfig eth0 | grep "inet addr" | cut -d ":" -f2 | cut -d " " -f1)
 CONF_DIR="$HOME/.ipython/profile_default"
+CONF_FILE="${CONF_DIR}/ipython_notebook_config.py"
+
 mkdir -p "${CONF_DIR}"
 
-CONF_FILE="${CONF_DIR}/ipython_notebook_config.py"
-PREFIX=/${DOMINO_PROJECT_OWNER}/${DOMINO_PROJECT_NAME}/notebookSession/${DOMINO_RUN_ID}/
-
-cat >> $CONF_FILE << EOF
+cat <<EOF >>"${CONF_FILE}"
 c = get_config()
-c.NotebookApp.notebook_dir = '${DRT_WORKING_DIR:-"/"}'
-c.NotebookApp.base_url = '${PREFIX}'
-c.NotebookApp.tornado_settings = {'headers': {'Content-Security-Policy': 'frame-ancestors *'}, 'static_url_prefix': '${PREFIX}static/'}
-c.NotebookApp.default_url = '/lab/tree${DOMINO_WORKING_DIR}'
-c.NotebookApp.token = u''
+c.NotebookApp.tornado_settings = {'headers': {'Content-Security-Policy': 'frame-ancestors *'}}
 EOF
 
-COMMAND='jupyter-lab --config="$CONF_FILE" --no-browser --ip="0.0.0.0" 2>&1'
-eval ${COMMAND} 
+# need to start jupyterlab in desired working directory
+# https://github.com/jupyterlab/jupyterlab/issues/2980
+COMMAND='cd ${DOMINO_WORKING_DIR}; jupyter-lab --config="$CONF_FILE" --no-browser --ip=* 2>&1'
+FINAL_COMMAND=$(echo "${COMMAND}" | sed "s/--ip=\\*/--ip=${IP_ADDR}/")
+
+eval ${FINAL_COMMAND}
